48c3d7e3e82fab3acd0b6f191d559e5719f2ae48
Linus Lagerhjelm <id14llm@cs.umu.se>
2015-09-08 14:53:24 +0200

    Update server socket

--- src/ChatServer.java
+++ src/ChatServer.java
@@ -1,5 +1,9 @@
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * Created by id14llm on 2015-09-08.
@@ -8,7 +12,20 @@ public class ChatServer {
     Integer IP;
     Integer port;
 
-    public ChatServer() throws IOException {
+    public ChatServer(Integer IP, Integer port) throws IOException {
+        this.IP = IP;
+        this.port = port;
+
+        ServerSocket serverSocket = new ServerSocket(IP, port);
+        Socket socket = serverSocket.accept();
+
+        InputStream in = socket.getInputStream();
+
+        Scanner scanner = new Scanner(in);
+
+    }
+
+    public void sendHeartBeat(){
 
     }
 }
